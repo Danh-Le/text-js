4ed73ca81d690674c0ae33dde3493549059392da
Danh-Le <quang.danh.le@gmail.com>
2022-06-20 14:08:13 +0200

    test done

---
+++ index.js
@@ -0,0 +1,35 @@
+// 01 - Hello Word
+console.log("Hello World !")
+
+// 02 - String
+const firstName = "Danh"
+console.log (firstName)
+
+// 03 - Concaténation
+const name = "Danh"
+console.log (`Nice to meet you ${name}`)
+
+// 04 - String Length
+const sentence = "I'm a new text!"
+console.log(sentence.length)
+
+// 05 - Replace
+let food = "croissant is meh"
+food = food.replace("meh", "so good")
+console.log(food)
+
+// 06 - Up and Down
+const basic = "This is cool"
+const basicUp = basic.toUpperCase()
+console.log(basicUp)
+const basicDown = basicUp.toLowerCase()
+console.log(basicDown)
+
+// 07 - Split
+const word = "banana"
+const letters = word.split("")
+console.log(letters)
+
+// 08 - Template
+const age = 34
+console.log(`I'm ${age} years old`)
